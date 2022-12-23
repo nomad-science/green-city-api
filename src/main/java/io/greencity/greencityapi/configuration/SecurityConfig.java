e305e21dcda883b2f5b88b108c4db8388a92ca08
Evan Lewis <evan.lewis084@gmail.com>
2022-12-23 15:43:31 -0500

    feature(setup): Add security and test features
    - Add security configuration to exclude public endpoint
    - Add integration test

---
+++ src/main/java/io/greencity/greencityapi/configuration/SecurityConfig.java
@@ -0,0 +1,21 @@
+package io.greencity.greencityapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain plantApiConfig(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.securityMatcher("/v1/**")
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
+
+        return httpSecurity.build();
+    }
+
+}
